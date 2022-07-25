29effb40f773b3abf9782bfff603da77cefd5c64
Nguyen Huy Manh <huymanh0698@gmail.com>
2022-07-25 16:45:28 +0700

    hihi

--- src/page/MoviePage.js
+++ src/page/MoviePage.js
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import useSWR from 'swr'
+import useSWR from 'swr';
+import ReactPaginate from 'react-paginate';
 import MovieCard from '../components/movie/MovieCard';
 import { fetcher } from '../config';
 import useDebounce from '../hooks/useDebounce';
 
+const itemsPerPage = 20;
+
 const MoviePage = () => {
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(1);
     const [filter, setFilter] = useState('');
-    const [url, setUrl] = useState('https://api.themoviedb.org/3/movie/popular?api_key=bdbdc442fa53c848e35c2a262310be7f');
+    const [url, setUrl] = useState(`https://api.themoviedb.org/3/movie/popular?api_key=bdbdc442fa53c848e35c2a262310be7f&page=${page}`);
     const filterDebounce = useDebounce(filter, 3000);
     const handleFilter = (e) => {
         setFilter(e.target.value);
     }
     useEffect(() => {
         if (filterDebounce) {
-            setUrl(`https://api.themoviedb.org/3/search/movie?api_key=bdbdc442fa53c848e35c2a262310be7f&query=${filterDebounce}`);
+            setUrl(`https://api.themoviedb.org/3/search/movie?api_key=bdbdc442fa53c848e35c2a262310be7f&query=${filterDebounce}&page=${page}`);
         } else {
-            setUrl('https://api.themoviedb.org/3/movie/popular?api_key=bdbdc442fa53c848e35c2a262310be7f');
+            setUrl(`https://api.themoviedb.org/3/movie/popular?api_key=bdbdc442fa53c848e35c2a262310be7f&page=${page}`);
         }
-    }, [filterDebounce]);
+    }, [filterDebounce, page]);
     const { data } = useSWR(url, fetcher);
     const loading = !data;
     const movies = data?.results || [];
+    useEffect(() => {
+        if (!data || !data.total_pages) return;
+        setPageCount(Math.ceil(data.total_pages / itemsPerPage));
+    }, [data]);
+
+    const handlePageClick = (event) => {
+        setPage(event.selected + 1);
+    };
+
     return (
         <div className="py-10 page-container">
             <div className="flex mb-10">
@@ -59,6 +73,25 @@ const MoviePage = () => {
                     ))
                 }
             </div>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={4}
+                marginPagesDisplayed={4}
+                pageCount={pageCount}
+                previousLabel="<"
+                renderOnZeroPageCount={null}
+                className="custom-paginate mt-20 flex justify-center"
+                pageClassName="mx-1 rounded-md border"
+                previousClassName="bg-blue-800 rounded-md text-white"
+                nextClassName="bg-blue-800 rounded-md text-white"
+                breakClassName="px-4 py-2"
+                activeClassName="bg-slate-500 text-white"
+                pageLinkClassName="px-4 py-2 flex justify-center"
+                previousLinkClassName="px-4 py-2 flex justify-center"
+                nextLinkClassName="px-4 py-2 flex justify-center"
+            />
         </div>
     );
 };
